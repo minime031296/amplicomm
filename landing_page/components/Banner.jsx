78fc5ea14a3ce1cd3a37acfa61391dbcd7f16911
mahak <minime031296@gmail.com>
2024-09-26 15:34:26 +0530

    final commit

--- landing_page/components/Banner.jsx
+++ landing_page/components/Banner.jsx
@@ -3,8 +3,9 @@ import React from 'react'
 
 const Banner = () => {
   return (
-    <Box sx={{margin: "10px 75px",border:"1px solid black", height:"70vh"}}>
-      <img src="https://www.icpahealth.com/wp-content/uploads/2023/11/home-slider-img-2-1.png" alt="" />
+    <Box sx={{margin: "10px 75px",border:"1px solid black", height:"20vh"}}>
+      <img src="https://healthyhey.com/cdn/shop/files/Website_Banner_with_Family_Desktop_2f0b36bf-405d-4c58-bd32-f4b49f6b4fe7.jpg?v=1716462023" alt="" 
+      style={{height:"100%", width:"100%", objectFit:"cover"}}/>
     </Box>
   )
 }
--- landing_page/src/App.jsx
+++ landing_page/src/App.jsx
@@ -14,6 +14,7 @@ import TitleFive from '../components/TitleFive'
 import BigCards from '../components/BigCards'
 import Youtube from '../components/Youtube'
 import Footer from '../components/Footer'
+import Banner from '../components/Banner'
 
 
 function App() {
@@ -29,6 +30,7 @@ function App() {
       <TitleTwo/>
       <Multicard/>
       <TitleThree/>
+      <Banner/>
       <ImagesCard/>
       <TitleFour/>
       <ImagesCardsTwo/>
